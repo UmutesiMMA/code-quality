8db06e995df26da117e314a1f3a62de560c7e38d
umutesimagnificat@gmail.com <umutesimagnificat@gmail.com>
2023-10-25 11:13:12 +0200

    sum calculation using  recursion

--- src/exercise4/sum.js
+++ src/exercise4/sum.js
@@ -1,4 +1,5 @@
 //Calculate sum of array elements
-function sum (array){
-    if(array.length<1)return 0
+function sum(array) {
+  if (array.length < 1) return 0;
+  return array[0] + sum(array.slice(1));
 }
