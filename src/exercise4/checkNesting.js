978322c5a37b055a2fec79c474f22f30f2d389c8
umutesimagnificat@gmail.com <umutesimagnificat@gmail.com>
2023-10-25 12:21:30 +0200

    refactored checkNesting function

--- src/exercise4/checkNesting.js
+++ src/exercise4/checkNesting.js
@@ -1,14 +1,14 @@
 function checkNestingLevel(arr) {
-  if (!Array.isArray(arr)) return 0;
-  let level =0;
-  for (let i of arr) {
-      if (Array.isArray(arr)) {
-        level++;
-      checkNestingLevel(i);
+    if (!Array.isArray(arr)) return 0;
+    let maxLevel=0
+    for (let i of arr) {
+      if (Array.isArray(i)) {
+        let currentLevel=checkNestingLevel(i);
+        maxLevel= Math.max(currentLevel,maxLevel)
+      }
     }
+    return maxLevel+1
   }
-  return level;
-}
-function flatten(arr) {
-  return arr.flat(checkNestingLevel(arr));
-}
+  function flatten(arr) {
+    return arr.flat(checkNestingLevel(arr));
+  }  
